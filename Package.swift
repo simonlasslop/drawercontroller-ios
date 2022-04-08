2738e78e1a040e5b65547f9af9c8d066cb06b44d
Lasslop, Simon <simon.lasslop@spiegel.de>
2022-04-08 11:31:31 +0200

    changed name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "MMDrawerController",
+    name: "DrawerController",
     platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MMDrawerController",
+            name: "DrawerController",
             targets: ["DrawerController"]),
     ],
     dependencies: [
